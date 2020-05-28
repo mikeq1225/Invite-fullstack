16f5e1f6c4c0b60d4efbc9adaacade0387dbe2f1
Michael Quarne <mikedq1225@gmail.com>
2020-05-28 15:27:17 -0700

    trying deployment

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require("express")
+const path = require("path")
 const userRoutes = require("./routes/user")
 
 const app = express()
-const port = 3001
 
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use("/api", userRoutes)
 
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function (req, res) {
+	res.sendFile(path.join(__dirname + "/public/index.html"))
+})
+
+const port = process.env.PORT || 3001
 app.listen(port, () => {
-  console.log(`LISTENING ON PORT ${port}`)
+	console.log(`LISTENING ON PORT ${port}`)
 })
---
+++ server/public/precache-manifest.adccf508b6a638ace47a3ea465ac1788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c5adc30573a1c4f02cb6d5264bda50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "737fbdc07c31e051c8f7",
+    "url": "/static/css/main.bdd3e50c.chunk.css"
+  },
+  {
+    "revision": "b3e519c0fbc30fafa26a",
+    "url": "/static/js/2.b2634636.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.b2634636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "737fbdc07c31e051c8f7",
+    "url": "/static/js/main.2320bc07.chunk.js"
+  },
+  {
+    "revision": "4789b4d78fc037fcebac",
+    "url": "/static/js/runtime-main.defac879.js"
+  }
+]);
